9cfe9cffb163aaceea52db17c255a668f99b60f8
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-06-19 16:16:41 -0300

    Adicionando display flex ao componente da logo e centralizando-o

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import styles from "./Logo.module.css";
 const Logo = () => {
     return (
+        
+        
         <a href="#">
             <div className={styles.logo}>
-            <img src="./imagens/logo.svg"alt="logo da library tech"></img>
-            <h1>LibraryTech</h1>
+                <div className={styles.imagem}>
+                    <img src="./imagens/logo.svg"alt="logo da library tech"></img>
+                </div>
+                <h1>LibraryTech</h1>
             </div>
         </a>
     );
