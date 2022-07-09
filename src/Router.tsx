7b5b056666aa1852f7dbd3bd8b6d38665692f195
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-07-09 12:21:42 -0300

    modifiyng router to accept url parameter slug

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import Design from "./pages/Design";
-import Frontend from "./pages/Frontend";
 import MainPage from "./pages/MainPage";
-import MaisVendidos from "./pages/MaisVendidos";
-import Programacao from "./pages/Programacao";
-
+import NotFound from "./pages/NotFound";
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/design" element={<Design />} />
-            <Route path="/front" element={<Frontend />} />
-            <Route path="/top-sellers" element={<MaisVendidos />} />
-            <Route path="/programation" element={<Programacao />} />
+            <Route path="/:slug"element={<MainPage/>}/>
         </Routes>
 
     );
