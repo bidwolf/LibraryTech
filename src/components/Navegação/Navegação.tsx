362a859eed61b31947f8d2f8ffeeca67e0867c41
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-07-09 11:25:05 -0300

    Add style to active links

--- src/components/Navegação/Navegação.tsx
+++ src/components/Navegação/Navegação.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import styles from "./Navegação.module.css";
-function Navegação() {
+interface INavLinkProps{
+    isActive: boolean;
+}
+const Navegação: React.FC = () => {
+    const navLinkStyles = ({ isActive }: INavLinkProps) => {
+        return {
+            textDecoration: isActive ? "underline" : "none",
+            fontWeight:isActive?"bold":"normal"
+        }
+    }
     return (
         <nav className={styles.navbar}>
             
 
             <ul>
-                <li><NavLink className={styles.navLink} to="/">Home</NavLink></li>
-                <li><NavLink className={styles.navLink} to="/front">Frontend</NavLink></li>
-                <li><NavLink className={styles.navLink} to="/programation">Programação</NavLink></li>
-                <li><NavLink className={styles.navLink} to="/design">Design</NavLink></li>
-                <li><NavLink className={styles.navLink} to='/top-sellers'>Mais vendidos</NavLink></li>
+                <li><NavLink className={styles.navLink} style={ navLinkStyles} to="/">Home</NavLink></li>
+                <li><NavLink className={styles.navLink} style={ navLinkStyles} to="/front">Frontend</NavLink></li>
+                <li><NavLink className={styles.navLink} style={ navLinkStyles} to="/programation">Programação</NavLink></li>
+                <li><NavLink className={styles.navLink} style={ navLinkStyles} to="/design">Design</NavLink></li>
+                <li><NavLink className={styles.navLink} style={ navLinkStyles} to='/top-sellers'>Mais vendidos</NavLink></li>
 
             </ul>
         
