38875f1731465556324ba4a19eff7dd7f06bd7c8
Henrique de Rodrigues <tec.henriquedepaula@gmail.com>
2022-07-09 12:18:41 -0300

    Switch page contents to components

---
+++ src/components/Books/Programacao.tsx
@@ -0,0 +1,11 @@
+function Programacao() {
+    return ( 
+        <main>
+            <h2>
+                Conteúdos sobre Programação
+            </h2>
+        </main>
+     );
+}
+
+export default Programacao;
